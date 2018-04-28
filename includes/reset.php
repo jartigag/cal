878f96511cdbcf0966121ff0681e5b3df4e79da8
jartigag <jartigag@protonmail.com>
2018-04-28 20:07:13 +0200

    mysql querys to reset tables

---
+++ includes/reset.php
@@ -0,0 +1,22 @@
+create table users (id int not null primary key auto_increment,
+ username varchar(40) not null,
+ email varchar(40),
+ tlmcoin int(2),
+ password char(40) not null,
+ random_salt char(40) not null);
+
+create table login_attempts (user_id int not null primary key,
+ date_time datetime not null);
+
+create table classes (id int not null primary key auto_increment,
+ course varchar(100) not null,
+ lesson varchar(100) not null,
+ price decimal(8,2) not null,
+ datetime_start datetime not null,
+ datetime_end datetime not null);
+
+create table events (id int not null primary key auto_increment,
+ date_time datetime not null,
+ class_id int not null,
+ user_id int not null,
+ teacher tinyint(1));
